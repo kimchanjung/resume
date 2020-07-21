b3fbc6b9c94ffab864d9bbb927d8173fb5d9097f
kimchanjung <mogomezwai@gmail.com>
2020-07-21 15:02:00 +0900

    프로필이미지 그림자 추가

--- component/profile/image.tsx
+++ component/profile/image.tsx
@@ -3,7 +3,12 @@ import { PropsWithChildren } from 'react';
 export default function ProfileImage({ src }: PropsWithChildren<{ src: string }>) {
   return (
     <div className="pb-3 text-md-right text-center">
-      <img style={{ maxHeight: '320px' }} className="img-fluid rounded" src={src} alt="Profile" />
+      <img
+        style={{ maxHeight: '320px', boxShadow: '5px 5px 5px #000;' }}
+        className="img-fluid rounded"
+        src={src}
+        alt="Profile"
+      />
     </div>
   );
 }

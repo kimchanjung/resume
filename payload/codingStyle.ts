8660902f17feb38e489fbb4984534d8351c42066
kimchanjung <mogomezwai@gmail.com>
2021-04-14 20:24:34 +0900

    코딩스타일 제목 수정

--- payload/codingStyle.ts
+++ payload/codingStyle.ts
@@ -2,7 +2,7 @@ import { IArticle } from '../component/article/IArticle';
 
 const article: IArticle.Payload = {
   disable: false,
-  title: '코딩스타일 참고 소스(github)',
+  title: '코딩스타일 참고 소스',
   list: [
     {
       content: '커머스(업소, 상품, 주문)도메인 기본 API',

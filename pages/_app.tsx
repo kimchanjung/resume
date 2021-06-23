54a9b692b97c6c5a6cd202be19e0ec40280efd86
kimchanjung <kimchanjung.dev@gmail.com>
2021-06-23 17:19:49 +0900

    페이지 랜더링 닫음

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,20 @@ import 'jquery/dist/jquery.slim';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../common.css';
 
-import { NextComponentType } from 'next';
+// import { NextComponentType } from 'next';
 
-export default function YosumeApp({
-  Component,
-  pageProps,
-}: {
-  Component: NextComponentType;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  pageProps: any;
-}) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+// export default function YosumeApp({
+//   Component,
+//   pageProps,
+// }: {
+//   Component: NextComponentType;
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   pageProps: any;
+// }) {
+//   // eslint-disable-next-line react/jsx-props-no-spreading
+//   return <Component {...pageProps} />;
+// }
+
+export default function YosumeApp() {
+  return null;
 }
